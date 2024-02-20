890420da0d1b32de6ebd617394cdcd7fef2d177d
faizan3165 <faizanmehmood3165@gmail.com>
2024-02-20 23:27:38 +0500

    implement sidebar and mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 
+import Sidebar from "@/components/shared/Sidebar";
+import MobileNav from "@/components/shared/MobileNav";
+
 import "../globals.css";
 
 export const metadata: Metadata = {
@@ -14,11 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <main className="root">
-      {/* sidebar */}
+      <Sidebar />
 
-      {/* mobile nav */}
+      <MobileNav />
 
-      
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
