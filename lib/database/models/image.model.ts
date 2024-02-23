34567baa53e8fadf47a93151070ddadc7a0b7d0b
faizan3165 <faizanmehmood3165@gmail.com>
2024-02-23 17:47:16 +0500

    create mongoose connection and models

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,91 @@
+import { Schema, model, models } from "mongoose";
+
+export interface IIMage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: string;
+  transformationUrl: string;
+  aspectRatio: string;
+  color: string;
+  prompt: string;
+  width: number;
+  height: number;
+  config?: object;
+  createdAt: Date;
+  updatedAt: Date;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+}
+
+const ImageSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  transformationType: {
+    type: String,
+    required: true,
+  },
+
+  publicId: {
+    type: String,
+    required: true,
+  },
+
+  secureUrl: {
+    type: URL,
+    required: true,
+  },
+
+  width: {
+    type: Number,
+  },
+
+  height: {
+    type: Number,
+  },
+
+  config: {
+    type: Object,
+  },
+
+  transformationUrl: {
+    type: URL,
+  },
+
+  aspectRatio: {
+    type: String,
+  },
+
+  color: {
+    type: String,
+  },
+
+  prompt: {
+    type: String,
+  },
+
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
