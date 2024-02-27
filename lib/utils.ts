6d476e99aaf09ff32ea9981e617ec869ac79c9f7
faizan3165 <faizanmehmood3165@gmail.com>
2024-02-27 22:05:52 +0500

    add tranformation form

--- lib/utils.ts
+++ lib/utils.ts
@@ -87,6 +87,7 @@ export function removeKeysFromQuery({
 // DEBOUNCE
 export const debounce = (func: (...args: any[]) => void, delay: number) => {
   let timeoutId: NodeJS.Timeout | null;
+
   return (...args: any[]) => {
     if (timeoutId) clearTimeout(timeoutId);
     timeoutId = setTimeout(() => func.apply(null, args), delay);
